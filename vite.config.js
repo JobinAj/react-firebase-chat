96ebd0e0f71577a29aa083b04000ac3995cfa5f5
Jobin Aj <jobinaj6361@gmail.com>
2025-02-22 12:29:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Expose to external devices
+    port: 5173,  // Ensure it runs on the expected port
+  }
 })
